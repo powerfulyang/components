bfabb64ae8c2bffb87e22f9a4a2168fa8f0916ea
powerfulyang <powerfulyang@qq.com>
2021-12-09 17:39:25 +0800

    chore: 其实没大动

--- src/stories/TooltipExample.tsx
+++ src/stories/TooltipExample.tsx
@@ -1,7 +1,7 @@
 import type { FC } from 'react';
 import React from 'react';
-import type { TooltipProps } from '@';
-import { Tooltip } from '@';
+import type { TooltipProps } from '../../dist/cjs';
+import { Tooltip } from '../../dist/cjs';
 
 export const TooltipExample: FC<TooltipProps> = ({ children, ...props }) => (
   <div style={{ marginTop: '4rem' }}>
