69c3e50c9962ec88358cb945a8d13acda218490e
powerfulyang <i@powerfulyang.com>
2022-03-09 21:55:00 +0800

    fix: 小问题

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,4 @@
 import './index.scss';
-import { isClient } from '@powerfulyang/utils';
-
-if (isClient) {
-  const scriptElem = document.createElement('script');
-  scriptElem.src = '//at.alicdn.com/t/font_178634_7m8rip6osz4.js';
-  document.body.appendChild(scriptElem);
-}
 
 export * from './assets';
 export * from './components';
