57180eee2cb126dd0b2752c705fecddb1300a39a
powerfulyang <i@powerfulyang.com>
2022-05-16 17:48:38 +0800

    fix: iconfont 交给最外层控制

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,8 +1,6 @@
 import brokenImg from './broken.gif';
-import transparentImg from './transparent.png';
 
 /**
  * base64 encoded images
  */
-const Assets = { brokenImg, transparentImg };
-export { Assets };
+export const Assets = { brokenImg };
--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -2,16 +2,9 @@ import type { FC } from 'react';
 import React, { memo } from 'react';
 import classNames from 'classnames';
 import './index.scss';
-import { isClient } from '@powerfulyang/utils';
 
 interface IconProps extends React.SVGProps<SVGSVGElement> {
   type: string;
-}
-
-if (isClient) {
-  const scriptElem = document.createElement('script');
-  scriptElem.src = '//at.alicdn.com/t/font_178634_7m8rip6osz4.js';
-  document.body.appendChild(scriptElem);
 }
 
 export const Icon: FC<IconProps> = memo(({ className, type, ...restProps }) => (
