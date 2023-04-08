5e63e53c23c5e6f83b4a6f19ac5b6516ca4ca648
powerfulyang <i@powerfulyang.com>
2023-04-08 10:40:37 +0800

    fix: vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   root: path.resolve(__dirname, 'site'),
   build: {
     outDir: path.resolve(__dirname, 'build'),
